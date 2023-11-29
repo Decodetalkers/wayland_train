6b9d3424aa412beabe4a55f183e352b344f26e17
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-11-29 14:20:46 +0800

    chore: add commit

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,21 @@ use wayland_protocols_wlr::layer_shell::v1::client::{
     zwlr_layer_surface_v1::{self, Anchor},
 };
 
+#[derive(Debug)]
 struct BaseState;
+
+// so interesting, it is just need to invoke once, it just used to get the globals
+impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for BaseState {
+    fn event(
+        _state: &mut Self,
+        _proxy: &wl_registry::WlRegistry,
+        _event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
+        _data: &GlobalListContents,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+    ) {
+    }
+}
 
 #[derive(Debug)]
 struct SecondState {
@@ -86,19 +100,6 @@ impl Dispatch<xdg_wm_base::XdgWmBase, ()> for SecondState {
         }
     }
 }
-
-impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for BaseState {
-    fn event(
-        _state: &mut Self,
-        _proxy: &wl_registry::WlRegistry,
-        _event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
-        _data: &GlobalListContents,
-        _conn: &Connection,
-        _qh: &wayland_client::QueueHandle<Self>,
-    ) {
-    }
-}
-
 impl Dispatch<xdg_surface::XdgSurface, ()> for SecondState {
     fn event(
         state: &mut Self,
@@ -190,27 +191,43 @@ delegate_noop!(SecondState: ignore ZwlrLayerShellV1); // it is simillar with xdg
 
 fn main() {
     let connection = Connection::connect_to_env().unwrap();
-    let (globals, _) = registry_queue_init::<BaseState>(&connection).unwrap();
+    let (globals, _) = registry_queue_init::<BaseState>(&connection).unwrap(); // We just need the
+                                                                               // global, the
+                                                                               // event_queue is
+                                                                               // not needed, we
+                                                                               // do not need
+                                                                               // BaseState after
+                                                                               // this anymore
 
     let mut state = SecondState::default();
 
     let mut event_queue = connection.new_event_queue::<SecondState>();
     let qh = event_queue.handle();
 
-    let wmcompositer = globals.bind::<WlCompositor, _, _>(&qh, 1..=5, ()).unwrap();
-    let wl_surface = wmcompositer.create_surface(&qh, ());
+    let wmcompositer = globals.bind::<WlCompositor, _, _>(&qh, 1..=5, ()).unwrap(); // so the first
+                                                                                    // thing is to
+                                                                                    // get WlCompositor
+    let wl_surface = wmcompositer.create_surface(&qh, ()); // and create a surface. if two or more,
+                                                           // we need to create more
 
     let shm = globals.bind::<WlShm, _, _>(&qh, 1..=1, ()).unwrap();
     globals.bind::<WlSeat, _, _>(&qh, 1..=1, ()).unwrap();
 
-    let _ = connection.display().get_registry(&qh, ());
+    let _ = connection.display().get_registry(&qh, ()); // so if you want WlOutput, you need to
+                                                        // register this
 
-    event_queue.blocking_dispatch(&mut state).unwrap();
+    event_queue.blocking_dispatch(&mut state).unwrap(); // then make a dispatch
 
     println!("Hello, world!, {:?}", wl_surface);
     println!("Hello, world!, {:?}", shm);
-    println!("Hello, world!, {:?}", state);
+    println!("Hello, world!, {:?}", state); // you will find you get the outputs, but if you do not
+                                            // do the step before, you get empty list
 
+    // so it is the same way, to get surface detach to protocol, first get the shell, like wmbase
+    // or layer_shell or session-shell, then get `surface` from the wl_surface you get before, and
+    // set it
+    // finally thing to remember is to commit the surface, make the shell to init.
+    let (init_w, init_h) = (320, 240);
     if true {
         // this example is ok for both xdg_surface and layer_shell
         let layer_shell = globals
@@ -226,7 +243,7 @@ fn main() {
         );
         layer.set_anchor(Anchor::Bottom | Anchor::Right | Anchor::Left | Anchor::Top);
         layer.set_keyboard_interactivity(zwlr_layer_surface_v1::KeyboardInteractivity::OnDemand);
-        layer.set_size(320, 240);
+        layer.set_size(init_w, init_w);
     } else {
         let xdg_wm_base = globals.bind::<XdgWmBase, _, _>(&qh, 1..=2, ()).unwrap();
         let xdg_surface = xdg_wm_base.get_xdg_surface(&wl_surface, &qh, ());
@@ -235,8 +252,11 @@ fn main() {
     }
 
     wl_surface.commit();
-    let (init_w, init_h) = (320, 240);
 
+    // so during the init Configure of the shell, a buffer, atleast a buffer is needed.
+    // and if you need to reconfigure it, you need to commit the wl_surface again
+    // so because this is just an example, so we just commit it once
+    // like if you want to reset anchor or KeyboardInteractivity or resize, commit is needed
     let mut file = tempfile::tempfile().unwrap();
     draw(&mut file, (init_w, init_h));
     let pool = shm.create_pool(file.as_fd(), (init_w * init_h * 4) as i32, &qh, ());
